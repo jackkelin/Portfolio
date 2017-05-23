cfbf80deecfa7c7627419240a750dd06d6b3bd56
Jackelin Herrera <j.herr.g@gmail.com>
2017-05-23 15:57:21 -0500

    style work svg

--- src/_scripts/preloader.js
+++ src/_scripts/preloader.js
@@ -1,24 +1,15 @@
 (function($){
   function init_slide_up(){
-    var $svg_desktop = $('.work-svg--desktop');
-    var $svg_tablet = $('.work-svg--tablet');
-    var $svg_mobile = $('.work-svg--mobile');
-    setTimeout(function(){
-      $svg_desktop.addClass('animate-slideUp');
-    },200);
-    setTimeout(function(){
-      $svg_tablet.addClass('animate-slideUp');
-    },700);
-    setTimeout(function(){
-      $svg_mobile.addClass('animate-slideUp');
-    },1200);
+    var $work_svg = $('.work-svg');
+    $work_svg.addClass('is_visible');
   }
   $(window).on('load', function(){
     setTimeout(function(){
       $('body').removeClass('preloading');
       $('.preloader').addClass('is_loaded');
-      init_slide_up();
+
     },500);
+    init_slide_up();
   });
 
 })(jQuery);
