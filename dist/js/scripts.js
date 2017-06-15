92c93b120a392ca24c246be345786dba23bb636d
Jackelin Herrera <j.herr.g@gmail.com>
2017-06-15 16:30:22 -0500

    jq - fix preloader timing

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1704,25 +1704,12 @@ This test will also return `true` for Firefox 4 Multitouch support.
     $('.preloader-icon').addClass('is_visible');
   });
   $(window).on('load', function(){
-    // $('.preloader-icon').removeClass('is_visible');
-    // $('body').removeClass('preloading');
-    // $('.preloader').addClass('is_loaded');
-
-    // setTimeout(function(){
-
-    // }, 500);
-    // setTimeout(function(){
-    //   $('body').removeClass('preloading');
-    //   $('.preloader').addClass('is_loaded');
-
-    // },900);
     setTimeout(function(){
-    $('.preloader-icon').removeClass('is_visible');
-    $('body').removeClass('preloading');
-    $('.preloader').addClass('is_loaded');
-    init_slide_up();
+      $('.preloader-icon').removeClass('is_visible');
+      $('body').removeClass('preloading');
+      $('.preloader').addClass('is_loaded');
+      init_slide_up();
     },1500);
-
   });
 
 })(jQuery);
--- src/_scripts/preloader.js
+++ src/_scripts/preloader.js
@@ -7,25 +7,12 @@
     $('.preloader-icon').addClass('is_visible');
   });
   $(window).on('load', function(){
-    // $('.preloader-icon').removeClass('is_visible');
-    // $('body').removeClass('preloading');
-    // $('.preloader').addClass('is_loaded');
-
-    // setTimeout(function(){
-
-    // }, 500);
-    // setTimeout(function(){
-    //   $('body').removeClass('preloading');
-    //   $('.preloader').addClass('is_loaded');
-
-    // },900);
     setTimeout(function(){
-    $('.preloader-icon').removeClass('is_visible');
-    $('body').removeClass('preloading');
-    $('.preloader').addClass('is_loaded');
-    init_slide_up();
+      $('.preloader-icon').removeClass('is_visible');
+      $('body').removeClass('preloading');
+      $('.preloader').addClass('is_loaded');
+      init_slide_up();
     },1500);
-
   });
 
 })(jQuery);
